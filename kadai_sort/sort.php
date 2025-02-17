226893e7cc7cfb1ab8f428ccb4177a20506243e8
rumale0508 <mkamiya1026@gmail.com>
2025-02-17 21:27:58 +0900

    commit kadai_sort

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -8,19 +8,29 @@
 <body>
     <p>
         <?php
-        $nums = [15, 4, 18, 23, 10];
+        function sort_2way(array $array, bool $order): array {
+            if ($order === true) {
+                sort($array);  // 昇順ソート
+            } else {                
+                rsort($array); // 降順ソート
+            }
+            return $array;
+        }
+
+        $array = [15, 4, 18, 23, 10];
+        
         echo '昇順にソートします。<br>';
-        sort($nums);        
-        foreach($nums as $num) {
+        $sortedAsc = sort_2way($array, true);
+        foreach ($sortedAsc as $num) {
             echo $num . '<br>';
         }
 
         echo '降順にソートします。<br>';
-        rsort($nums);        
-        foreach($nums as $num) {
+        $sortedDesc = sort_2way($array, false);
+        foreach ($sortedDesc as $num) {
             echo $num . '<br>';
         }
         ?>
-    </p>    
+    </p>
 </body>
 </html>
